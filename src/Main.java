e6406ffea83f77b74968b483d4b5ed27cd03b971
KseniiaDovganiuk <50490901+KseniiaDovganiuk@users.noreply.github.com>
2024-09-19 13:48:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int ticket_cost = 1000; // стоимость билета
+        int bonus_rate = 20; // количество рублей для одной бонусной мили
+        int bonus_miles = ticket_cost / bonus_rate; // количество бонусных миль
+
+        System.out.println(bonus_miles + " бонусных миль");
+
     }
 }
